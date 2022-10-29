a56d5db2e018eefac8e607958160229c971e46bf
David Kobakhidze <dakobak@gmail.com>
2022-10-29 17:11:36 +0100

    Ya cool hacker

--- src/contractInteractions/Treeco.js
+++ src/contractInteractions/Treeco.js
@@ -1,7 +1,7 @@
 
 import WalletConnectModal from './walletConnect/walletConnectModal';
 import * as React from "react";
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
@@ -13,10 +13,10 @@ const GREETER_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 export class Treeco extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        greeting: "",
-      }
+    super(props);
+    this.state = {
+      greeting: "",
+    }
   }
 
   async fetchGreeting() {
@@ -26,53 +26,54 @@ export class Treeco extends React.Component {
       const contract = new ethers.Contract(GREETER_ADDRESS, Greeter.abi, provider);
       //try to get the greeting in the contract
       try {
-          const data = await contract.greet();
-          this.setState({greeting: data});
-          console.log("Data: ", data);
+        const data = await contract.greet();
+        this.setState({ greeting: data });
+        console.log("Data: ", data);
       } catch (e) {
-          console.log("Err: ", e)
+        console.log("Err: ", e)
       }
     }
-}
+  }
 
-async setGreeting(newGreeting) {
+  async setGreeting(newGreeting) {
     if (newGreeting && typeof window.ethereum !== "undefined") {
-        //ethereum is usable, get reference to the contract
-        await this.requestAccount();
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
+      //ethereum is usable, get reference to the contract
+      await this.requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-        //signer needed for transaction that changes state
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(GREETER_ADDRESS, Greeter.abi, signer);
+      //signer needed for transaction that changes state
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(GREETER_ADDRESS, Greeter.abi, signer);
 
-        //preform transaction
-        const transaction = await contract.setGreeting(newGreeting);
-        await transaction.wait();
-        this.fetchGreeting();
+      //perform transaction
+      const transaction = await contract.setGreeting(newGreeting);
+      await transaction.wait();
+      this.fetchGreeting();
     }
-}
+  }
 
   async requestAccount() {
-    await window.ethereum.request({method: 'eth_requestAccounts'});
-}
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
 
   render() {
     return (
       <Stack
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
       >
         <Typography variant='h1'>Greeting</Typography>
-        <Button onClick={() => this.fetchGreeting()}>
+        <Typography variant='h2'>Current "Greet": {this.state.greeting} </Typography>
+        {<Button onClick={() => this.fetchGreeting()}>
           Get Greeting
-        </Button>
-            <input id={"new-greeting"} placeholder={"New Greeting"}/>
-            <button onClick={() => {
-                const newGreeting = document.getElementById("new-greeting").value;
-                this.setGreeting(newGreeting);
-            }}>Update Greeting</button>
-            <WalletConnectModal/>
+        </Button>}
+        <input id={"new-greeting"} placeholder={"New Greeting"} />
+        <button onClick={() => {
+          const newGreeting = document.getElementById("new-greeting").value;
+          this.setGreeting(newGreeting);
+        }}>Update Greeting</button>
+        <WalletConnectModal />
       </Stack>
     )
   }
