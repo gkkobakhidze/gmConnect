bb4edca4688cf41da0aeba602d5aa9c5896405f0
George <gkkobakhidze@gmail.com>
2022-10-30 10:48:27 +0000

    Small change to the URLs

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
       <Route path="" element= {<TreecoLayout/> } >
         <Route path="" element ={<Navigate to="/gm" replace/>} />
         <Route path="/gm" element= {<TreecoScanMe/> } />
+        <Route path="/bd" element= {<TreecoScan/> } />
         <Route path="/bd/:connection" element= {<TreecoScan/> } />
         <Route path="/journey" element= {<Journey/> } />
       </Route>
